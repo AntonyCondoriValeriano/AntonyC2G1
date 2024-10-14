00888ef359605cd8916b6f85301f85d58fc8dca9
AntonyCondoriValeriano <antonycondorivaleriano87@gmail.com>
2024-10-13 21:31:43 -0500

    crud en consola

--- SysAlmacenFX/src/main/java/pe/edu/upeu/sysalmacenfx/pruebas/MainX.java
+++ SysAlmacenFX/src/main/java/pe/edu/upeu/sysalmacenfx/pruebas/MainX.java
@@ -2,7 +2,6 @@ package pe.edu.upeu.sysalmacenfx.pruebas;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import pe.edu.upeu.sysalmacenfx.SysAlmacenFxApplication;
 import pe.edu.upeu.sysalmacenfx.modelo.Categoria;
 import pe.edu.upeu.sysalmacenfx.repositorio.CategoriaRepository;
 
@@ -13,42 +12,79 @@ import java.util.Scanner;
 public class MainX {
     @Autowired
     CategoriaRepository repository;
-    public void registro(){
+
+    public void registro() {
         System.out.println("MAIN CATEGORIA");
-        Categoria ca=new Categoria();
+        Categoria ca = new Categoria();
         ca.setNombre("Utiles escritorio");
-        Categoria dd=repository.save(ca);
+        Categoria dd = repository.save(ca);
         System.out.println("Reporte:");
-        System.out.println(dd.getIdCategoria() + "  "+ dd.getNombre());
+        System.out.println(dd.getIdCategoria() + "  " + dd.getNombre());
     }
 
-    public void listar(){
-        List<Categoria> datos=repository.findAll();
+    public void listar() {
+        List<Categoria> datos = repository.findAll();
         System.out.println("ID\tNombre");
-        for (Categoria ca: datos) {
-            System.out.println(ca.getIdCategoria()+"\t"+ca.getNombre());
+        for (Categoria ca : datos) {
+            System.out.println(ca.getIdCategoria() + "\t" + ca.getNombre());
         }
     }
 
+    public void update() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese ID de la categoría a actualizar: ");
+        Long id = Long.parseLong(sc.nextLine());
 
-    public void menu(){
-        int opc=0;
-        Scanner cs=new Scanner(System.in);
-        String mensaje="Seleccione una opcion: \n 1=Crear\n2=Listar\n0=Salir";
-        System.out.println(mensaje);
-        opc=Integer.parseInt(cs.next());
-        do {
-            switch (opc){
-                case 1:{registro();}
-                case 2:{listar();}
-            }
-            System.out.println(mensaje);
-            opc=Integer.parseInt(cs.next());
-        }while(opc!=0);
+        Categoria ca = repository.findById(id).orElse(null);
+        if (ca != null) {
+            System.out.print("Ingrese nuevo nombre: ");
+            String nuevoNombre = sc.nextLine();
+            ca.setNombre(nuevoNombre);
+            repository.save(ca);
+            System.out.println("Categoría actualizada: " + ca.getIdCategoria() + "  " + ca.getNombre());
+        } else {
+            System.out.println("Categoría no encontrada con ID: " + id);
+        }
     }
 
+    public void delete() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese ID de la categoría a eliminar: ");
+        Long id = Long.parseLong(sc.nextLine());
 
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            System.out.println("Categoría eliminada con ID: " + id);
+        } else {
+            System.out.println("Categoría no encontrada con ID: " + id);
+        }
+    }
+
+    public void menu() {
+        int opc = 0;
+        Scanner cs = new Scanner(System.in);
+        String mensaje = "Seleccione una opción: \n1 = Crear\n2 = Listar\n3 = Actualizar\n4 = Eliminar\n0 = Salir";
+
+        do {
+            System.out.println(mensaje);
+            opc = Integer.parseInt(cs.next());
+
+            switch (opc) {
+                case 1:
+                    registro();
+                    break;
+                case 2:
+                    listar();
+                    break;
+                case 3:
+                    update();
+                    break;
+                case 4:
+                    delete();
+                    break;
+                default:
+                    System.out.println("Opción inválida. Intente de nuevo.");
+            }
+        } while (opc != 0);
+    }
 }
-
-
-
